eaf876427991485dd31bd248d8b6d9783c0ec736
FarhanMirzah <62887690+FarhanMirzah@users.noreply.github.com>
2022-04-09 13:33:25 +0700

    Tambah query SELECT FROM users
    Tes di localhost:3000/database di browser atau Postman (gunakan GET)
    Akan ditampilkan data kolom id, name, email, dan type dari tabel users (dari database loki)

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const mysql=require('mysql')
+const mysql=require('mysql2')
 
 router.get('/database', function(req, res, next){
 	const connection=mysql.createConnection({
@@ -10,6 +10,12 @@ router.get('/database', function(req, res, next){
 		database:'loki'
 	});
 	connection.connect();
-	console.log("Berhasil connect ke database loki")
+	console.log("Berhasil connect ke database loki") // Lihat di CMD
+	
+	// Tes di localhost:3000/database (ditampilkan data dari tabel users)
+	connection.query('SELECT id, name, email, type FROM users', function(err, rows, fields){
+		if (err) throw err;
+		res.json(rows);
+	})
 });
 module.exports = router;
