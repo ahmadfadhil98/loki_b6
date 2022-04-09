ac322d91122dd68f3c59c787ab9f83aac152da92
FarhanMirzah <62887690+FarhanMirzah@users.noreply.github.com>
2022-04-09 16:48:18 +0700

    database jadi databaseRouter
    const databaseRouter = require('./database');
    
    // Router database (http://localhost:3000/database)
    app.use("/", databaseRouter);

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const database = require('./database');
+const databaseRouter = require('./database');
 
 const port = 3000
 
@@ -10,8 +10,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// Route database (http://localhost:3000/database)
-app.use("", database);
+// Router database (http://localhost:3000/database)
+app.use("/", databaseRouter);
 
 // Bagian Farhan (nomor 1 - 5)
 // Nomor 1. Login
