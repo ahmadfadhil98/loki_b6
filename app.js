de46d8cf8586086a686b421d6701c7ded5dd4574
NadillaSaraswati <90832058+NadillaSaraswati@users.noreply.github.com>
2022-06-29 22:11:49 +0700

    yyy

--- app.js
+++ app.js
@@ -23,28 +23,6 @@ app.get('/login',(req,res)=>{
   res.render(__dirname + '/views/login.ejs')
 })
 
-<<<<<<< HEAD
-=======
-app.get('/tambahRPS',(req,res)=>{
-  res.render(__dirname + '/views/dosen/tambahRPS.ejs')
-})
-
-app.get('/hasilPencarian',(req,res)=>{
-  res.render(__dirname + '/views/mahasiswa/hasilPencarian.ejs')
-})
-
-app.get('/dashboardMahasiswa',(req,res)=>{
-  res.render(__dirname + '/views/mahasiswa/dashboardMahasiswa.ejs')
-})
-
-app.get('/dosen/dashboardDosen',(req,res)=>{
-  res.render(__dirname + '/views/dosen/dashboardDosen.ejs')
-})
-
-app.get('/editkomponenPenilaian',(req,res)=>{
-  res.render(__dirname + '/views/dosen/editkomponenDosen.ejs')
-})
->>>>>>> 5afdc2d17f60f3c2184e992ce5bd846d9bdf52e7
 
 app.get('/Pemrograman_Web/detailRPS',(req,res)=>{
   res.render(__dirname + '/views/mahasiswa/Pemrograman_Web/detailRPS.ejs')
